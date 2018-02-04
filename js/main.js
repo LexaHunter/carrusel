95acd1bd7c2d2939fb80316dda73d5b572d6301b
Paz <pazrubiorubio@gmail.com>
2018-02-04 03:05:34 +0100

    carrusel-scroll-menu

---
+++ js/main.js
@@ -0,0 +1,142 @@
+$(document).ready(function(){
+
+	//SCROLL
+	var $animation_elements = $('.animation-element');
+	var $window = $(window);
+
+	function check_if_in_view() {
+	  var window_height = $window.height();
+	  var window_top_position = $window.scrollTop();
+	  var window_bottom_position = (window_top_position + window_height);
+	 
+	  $.each($animation_elements, function() {
+	    var $element = $(this);
+	    var element_height = $element.outerHeight();
+	    var element_top_position = $element.offset().top;
+	    var element_bottom_position = (element_top_position + element_height);
+	 
+	    //check to see if this current container is within viewport
+	    if ((element_bottom_position >= window_top_position) &&
+	        (element_top_position <= window_bottom_position)) {
+	      $element.addClass('in-view');
+	    } else {
+	      $element.removeClass('in-view');
+	    }
+	  });
+	}
+
+	$window.on('scroll resize', check_if_in_view);
+	$window.trigger('scroll');
+
+	// CARRUSEL
+	var imgItems = $('.slider li').length; // Numero de Slides
+	var imgPos = 1;
+
+	// Agregando paginacion --
+	for(i = 1; i <= imgItems; i++){
+		$('.pagination').append('<li><span class="fa fa-circle"></span></li>');
+	} 
+	//------------------------
+
+	$('.slider li').hide(); // Ocultanos todos los slides
+	$('.slider li:first').show(); // Mostramos el primer slide
+	$('.pagination li:first').css({'color': '#CD6E2E'}); // Damos estilos al primer item de la paginacion
+
+	// Ejecutamos todas las funciones
+	$('.pagination li').click(pagination);
+	$('.right span').click(nextSlider);
+	$('.left span').click(prevSlider);
+
+
+	setInterval(function(){
+		nextSlider();
+	}, 10000);
+
+	// FUNCIONES =========================================================
+
+	function pagination(){
+		var paginationPos = $(this).index() + 1; // Posicion de la paginacion seleccionada
+
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ paginationPos +')').fadeIn(); // Mostramos el Slide seleccionado
+
+		// Dandole estilos a la paginacion seleccionada
+		$('.pagination li').css({'color': '#858585'});
+		$(this).css({'color': '#CD6E2E'});
+
+		imgPos = paginationPos;
+
+	}
+
+	function nextSlider(){
+		if( imgPos >= imgItems){imgPos = 1;} 
+		else {imgPos++;}
+
+		$('.pagination li').css({'color': '#858585'});
+		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
+
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
+
+	}
+
+	function prevSlider(){
+		if( imgPos <= 1){imgPos = imgItems;} 
+		else {imgPos--;}
+
+		$('.pagination li').css({'color': '#858585'});
+		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
+
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
+	}
+
+});
+
+// Menu fijo al hacer scroll
+$(window).scroll(function(){
+
+    if ($(this).scrollTop() > 350){
+		 $('.menu').addClass("fijo").fadeIn();
+	}
+    else {
+		$('.menu').removeClass("fijo");
+	}
+});
+
+
+// cookies
+function getCookie(c_name){
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1){
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1){
+        c_value = null;
+    }else{
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1){
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start,c_end));
+    }
+    return c_value;
+}
+ 
+function setCookie(c_name,value,exdays){
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=c_name + "=" + c_value;
+}
+ 
+if(getCookie('tiendaaviso')!="1"){
+    document.getElementById("barraaceptacion").style.display="block";
+}
+function PonerCookie(){
+    setCookie('tiendaaviso','1',365);
+    document.getElementById("barraaceptacion").style.display="none";
+}
+
